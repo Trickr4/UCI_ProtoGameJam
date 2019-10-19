6231f820008ae2dde35687d0f866afb1eaab9bc4
Jonathan Chhou <chhouj@uci.edu>
2019-10-19 16:04:22 -0700

    Updating Player_input

--- Assets/Scripts/Player_Input.cs
+++ Assets/Scripts/Player_Input.cs
@@ -4,44 +4,74 @@ using UnityEngine;
 
 public class Player_Input : MonoBehaviour
 {
-    Queue<KeyCode> First_inputs;
-    Queue<KeyCode> Second_inputs;
+    Queue<KeyCode> First_inputs = new Queue<KeyCode>();
+    Queue<KeyCode> Second_inputs= new Queue<KeyCode>();
     private int Screen_size;
-    private int space;
-    Vector2 force;
-    GameObject Player1;
-    GameObject Player2;
+    private float space;
+
+    IEnumerator timer;
+
+    Rigidbody2D Player1_rb;
+    Rigidbody2D Player2_rb;
+    [SerializeField] float speed = 10f;
+    //Pass in the player object 
+    [SerializeField] GameObject Player1;
+    [SerializeField] GameObject Player2;
 
     // Start is called before the first frame update
     void Start()
     {
-        Screen_size = Screen.width;
-        space = (13 / 16) * Screen_size;
-    }
+        Player1_rb = Player1.GetComponent<Rigidbody2D>();
+        Player2_rb = Player2.GetComponent<Rigidbody2D>();
 
-    // Update is called once per frame
+
+        Screen_size = Screen.width;
+        space = Screen_size - ((13f/ 16f) * Screen_size);
+    }
+/*
+    if (rb.position.x<rb.position.x + space)
+    {
+        Debug.Log("heh");
+        rb.velocity = new Vector2(1, 0) * 0;
+        break;
+    }
+*/
+// Update is called once per frame
     void Update()
     {
+
         Get_Input();
         if (First_inputs.Count == 3 && Second_inputs.Count == 3)
             while (First_inputs.Count != 0 || Second_inputs.Count != 0)
             {
-                Move(First_inputs, Player1);
-                Move(Second_inputs, Player2);
+                Move(First_inputs, Player1_rb);
+                timer = WaitforStop(Player1_rb);
+                Move(Second_inputs, Player2_rb);
+                timer = WaitforStop(Player1_rb);
+                Player2_rb.velocity = new Vector2(1, 0) * 0;
             }
     }
 
-    void Move(Queue<KeyCode> inputs, GameObject player )
+    IEnumerator WaitforStop(Rigidbody2D rb)
+    {
+        Player1_rb.velocity = new Vector2(1, 0) * 0;
+        yield return new WaitForSeconds(0.5f);
+    }
+
+    void Move(Queue<KeyCode> inputs, Rigidbody2D rb )
     {
         KeyCode direction = inputs.Dequeue();
-        if (direction == KeyCode.W || direction == KeyCode.UpArrow)
-            player.transform.Translate(new Vector2(0, 1) * space );
+        if (direction == KeyCode.W || direction == KeyCode.UpArrow) 
+           rb.velocity = new Vector2(0, 1) * speed;
         if (direction == KeyCode.S || direction == KeyCode.DownArrow)
-            player.transform.Translate(new Vector2(0, -1) * space);
+            rb.velocity = new Vector2(0, -1) * speed;
         if (direction == KeyCode.D || direction == KeyCode.RightArrow)
-            player.transform.Translate(new Vector2(1, 0) * space);
+        {
+            rb.velocity = new Vector2(1, 0) * speed;
+        }
         if (direction == KeyCode.A || direction == KeyCode.LeftArrow)
-            player.transform.Translate(new Vector2(-1, 0) * space);
+            rb.velocity = new Vector2(-1, 0) * speed;
+        
     }
 
     void Get_Input()
