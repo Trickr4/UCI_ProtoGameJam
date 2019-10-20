427316d8455b207e3f0b8c467bb94812dcba4171
michacg <michacg2@uci.edu>
2019-10-19 20:52:05 -0700

    3 prefabs with stats

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    [SerializeField] int attack;
+    [SerializeField] int move;
+    [SerializeField] int defense;
+}
+
